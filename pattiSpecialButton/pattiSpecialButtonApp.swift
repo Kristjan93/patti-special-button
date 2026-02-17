29264d1ed70a3bcde272a52e74fc7fe19995faea
Kristjan93 <krillinn@gmail.com>
2026-02-17 16:04:18 +0000

    Initial Commit

---
+++ pattiSpecialButton/pattiSpecialButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  pattiSpecialButtonApp.swift
+//  pattiSpecialButton
+//
+//  Created by Kristjan Thorsteinsson on 17.2.2026.
+//
+
+import SwiftUI
+
+@main
+struct pattiSpecialButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pattiSpecialButtonTests/pattiSpecialButtonTests.swift
@@ -0,0 +1,17 @@
+//
+//  pattiSpecialButtonTests.swift
+//  pattiSpecialButtonTests
+//
+//  Created by Kristjan Thorsteinsson on 17.2.2026.
+//
+
+import Testing
+@testable import pattiSpecialButton
+
+struct pattiSpecialButtonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
